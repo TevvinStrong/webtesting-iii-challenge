7889f62f27c09ed390cd00387dafd9ae8051e1c2
Tevvin <Tevvin7395@icloud.com>
2019-06-26 14:53:10 -0400

    controls test added

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,9 @@
 // Test away!
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, fireEvent } from 'react-testing-library';
+
+import 'jest-dom/extend-expect';
 
 import Controls from './Controls.js';
 
@@ -10,4 +13,28 @@ describe('<Controls />', () => {
 
         expect(tree.toJSON()).toMatchSnapshot();
     });
+
+    // it('the lock toggle button is disabled if the gate is open', () => {
+    //     const { getByText } = render(<Controls closed={false} />)
+
+    //     expect(getByText(/lock gate/i)).toBeDisabled();
+    // })
+
+    // it('the close toggle button is disabled if the gate is locked', () => {
+    //     const { getByText } = render(<Controls locked={true} />)
+
+    //     expect(getByText(/close gate/i)).toBeDisabled();
+    // })
+
+    it('button text reflects state if clicked', () => {
+        const { getByText } = render(<Controls />)
+
+        const toggleClose = getByText(/close gate/i);
+        fireEvent.click(toggleClose);
+
+        expect(getByText(/close gate/i)).toHaveTextContent('Open Gate');
+
+    })
+
 });
+
