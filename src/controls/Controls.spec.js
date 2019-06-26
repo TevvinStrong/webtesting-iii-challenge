1a55d204e6d77d029e83093502c2b7e33173b593
Tevvin <Tevvin7395@icloud.com>
2019-06-25 21:55:39 -0400

    snapshot teest on each component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Controls from './Controls.js';
+
+describe('<Controls />', () => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Controls />);
+
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Display from './Display.js';
+
+describe('<Display />', () => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Display />);
+
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+});
